2befb9c2b6e41e3dae0813baf972db8746eefe43
Bhanu Prakash Maruboyina <bhanu.bm.maruboyina@fau.de>
2025-07-24 12:09:56 +0200

    Edit wave.cpp

--- physsim/8_ocean/wave.cpp
+++ physsim/8_ocean/wave.cpp
@@ -17,6 +17,13 @@ namespace physsim
     Eigen::Vector3d Wave::offset(const Eigen::Vector2d& position, const double& t) const
     {
         // TODO: compute the offset for the displaced wave from the given position.
-        return Eigen::Vector3d(0, 0, 0);
+        double phase = waveVector().dot(position) - waveNumber() *dispersion.phaseSpeed(waveNumber()) *t + 0.0f;
+
+        double eta = amplitude * std::cos(phase);
+
+        Eigen::Vector2d horiz_disp = -0.5f * (waveVector()/waveNumber() ) * amplitude * std::sin(phase);
+
+        return Eigen::Vector3d(horiz_disp.x(), horiz_disp.y(), eta);
+        // return Eigen::Vector3d(0, 0, eta);
     }
 }
