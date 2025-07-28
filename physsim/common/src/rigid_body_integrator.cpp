32ea00eafb3c615cb7f20852840797787a5b9bc1
bhanuPrakashMa <147130893+bhanuPrakashMa@users.noreply.github.com>
2025-07-28 13:39:16 +0200

    Update rigid_body_integrator.cpp

--- physsim/common/src/rigid_body_integrator.cpp
+++ physsim/common/src/rigid_body_integrator.cpp
@@ -140,5 +140,45 @@ namespace physsim
         // TODO: update position of the body using the linear velocity and update body accordingly
 
         // TODO: quaternion-based angular velocity update of rotation
+
+                // get current position and rotation of the body
+        Eigen::Vector3d x    = body.position();
+        Eigen::Quaterniond q = body.rotation();
+
+        // get current linear and angular velocity of the body
+        Eigen::Vector3d v = body.linearVelocity();
+        Eigen::Vector3d w = body.angularVelocity();
+
+        // update position of the body using the linear velocity and update body accordingly
+        Eigen::Vector3d xnew = x + stepSize * v;
+        body.setPosition(xnew);
+
+        // quaternion-based angular velocity update of rotation and update body accordingly
+        Eigen::Quaterniond wq(0, w.x(), w.y(), w.z());
+        Eigen::Quaterniond qnew = (q + 0.5 * stepSize * wq * q).normalized();
+        body.setRotation(qnew);
+
+        // get current linear and angular momentum of body
+        Eigen::Vector3d p = body.linearMomentum();
+        Eigen::Vector3d l = body.angularMomentum();
+
+        // get force and torque that are currently applied to body
+        Eigen::Vector3d f = body.force();
+        Eigen::Vector3d t = body.torque();
+
+        // compute new linear momentum
+        Eigen::Vector3d pnew = p + stepSize * f;
+
+        // convert from linear momentum to linear velocity and update the body accordingly
+        Eigen::Vector3d vnew = body.massInverse() * pnew;
+        body.setLinearVelocity(vnew);
+
+        // compute new angular momentum
+        Eigen::Matrix3d I    = body.inertiaWorld();
+        Eigen::Vector3d lnew = l + stepSize * (t - w.cross(I * w));
+
+        // convert from angular momentum to angular velocity and update the body accordingly
+        Eigen::Vector3d wnew = body.inertiaWorldInverse() * lnew;
+        body.setAngularVelocity(wnew);
     }
 }
